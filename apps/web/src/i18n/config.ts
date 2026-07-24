f8fe401298621d18afb5bef9f110e3ff843d61ae
Anxogcd <anxogcd@gmail.com>
2026-07-24 23:44:54 +0200

    feat(web): add i18n with ES/GL translations

---
+++ apps/web/src/i18n/config.ts
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import es from "./locales/es.json";
+import gl from "./locales/gl.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      es: { translation: es },
+      gl: { translation: gl },
+    },
+    fallbackLng: "gl",
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+  });
+
+export default i18n;
